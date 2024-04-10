e5aece21cdaa05fb3cc5858b10e359427f8e55d8
Oliver Tacke <o.tacke@posteo.de>
2024-04-10 17:35:12 +0200

    Modernize build chain

--- src/entries/dist.js
+++ src/entries/dist.js
@@ -1,4 +1,4 @@
-import InfoWallPassiveList from '@scripts/h5peditor-info-wall-passive-list';
+import InfoWallPassiveList from '@scripts/h5peditor-info-wall-passive-list.js';
 
 // Load library
 H5PEditor.InfoWallPassiveList = InfoWallPassiveList;
